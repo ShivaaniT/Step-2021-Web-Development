310558c15eb35cd071070ac1c63b22a94976c8f6
ShivaaniT <87347939+ShivaaniT@users.noreply.github.com>
2021-07-15 08:06:25 +0530

    Add files via upload

---
+++ Coding-Exe-18_String-Methods.js
@@ -0,0 +1,6 @@
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+
+// YOUR CODE BELOW THIS LINE:
+//Lowercase version of the string with no extra space at the end and beginning!
+let whisper=message.trim().toLowerCase()
