70faadedf47d0a7d96b4bb5c52934ba453630b3c
ShivaaniT <87347939+ShivaaniT@users.noreply.github.com>
2021-07-15 21:10:54 +0530

    Add files via upload

---
+++ Coding-Exe-21_Our-First-Conditional.js
@@ -0,0 +1,9 @@
+let num=20;
+//Checking if the number is even!
+function isEven(num){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+    if(num%2==0){
+        console.log("even");
+    }
+    //AND THIS LINE ↑↑↑↑↑
+}
---
+++ Coding-Exe-31_Our-First-For-Loop.js
@@ -0,0 +1,5 @@
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+let string="Da ba dee da ba daa";
+for(let i=1;i<=6;i++){
+    console.log(string);
+}
