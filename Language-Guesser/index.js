999954d25341398e60f8509f7f2f1d392fd06d7f
ShivaaniT <87347939+ShivaaniT@users.noreply.github.com>
2021-07-22 15:51:18 +0530

    Update index.js

--- Language-Guesser/index.js
+++ Language-Guesser/index.js
@@ -1,1 +1,14 @@
+const franc=require("franc");
+const langs=require("langs");
+const colors=require("colors");
 
+const input=process.argv[2];
+const langCode=franc(input);
+
+if(langCode=='und'){
+    console.log("Couldn't Guess!".red);
+}
+else{
+    const language=langs.where('3',langCode);
+    console.log(`Guess:${language.name}`.green);
+}
